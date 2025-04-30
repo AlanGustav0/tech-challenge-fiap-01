c0ae55d5eadd2b3022be33cbd14dd1d39b03a85d
alanziit <alanseverino05@gmail.com>
2025-04-29 23:05:59 -0300

    inclusao do bloco de saldo

--- src/app/pageUser/userPageHome.js
+++ src/app/pageUser/userPageHome.js
@@ -1,15 +1,29 @@
 'use client';
 
 import React, { useState } from 'react';
-import Image from 'next/image'; // Ajuste conforme necessário
-import './userPageHome.css'; // Estilos
+import Image from 'next/image';
+import './userPageHome.css'; 
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const UserPageHome = () => {
     const [menuVisible, setMenuVisible] = useState(false);
+    const [isBalanceVisible, setIsBalanceVisible] = useState(false);
 
     const toggleMenu = () => {
         setMenuVisible(!menuVisible);
     };
+    
+    const toggleBalanceVisibility = () => {
+        setIsBalanceVisible(!isBalanceVisible);
+    };
+
+   const balance = 2500.00;
+
+   const getCurrentDate = () => {
+    const now = new Date();
+    const options = { weekday: 'long', day: '2-digit', month: '2-digit', year: 'numeric' };
+    return now.toLocaleDateString('pt-BR', options);
+};
 
     return (
         <div className="user-page">
@@ -22,7 +36,7 @@ const UserPageHome = () => {
                         alt="User Icon" 
                         width={40} 
                         height={40} 
-                        onClick={toggleMenu} // Abre/fecha o menu ao clicar
+                        onClick={toggleMenu} 
                     />
                 </div>
             </div>
@@ -53,8 +67,22 @@ const UserPageHome = () => {
                     </ul>
                 </div>
                 <div className="main-content">
-                    <h1>Bem-vindo à Página do Usuário</h1>
-                    {/* Outros conteúdos da página */}
+                <div className="balance-card">
+                    <h2>Olá, Joana! :)</h2>
+                    <p>{getCurrentDate()}</p> 
+                    <div className="balance-info">
+                        <span>Saldo</span>
+                        <button onClick={toggleBalanceVisibility}>
+                            {isBalanceVisible ? <FaEye /> : <FaEyeSlash />}
+                        </button>
+                    </div>
+                    <p className='contaCorrente'>
+                        Conta Corrente
+                    </p>
+                    <p className='valorSaldo'>
+    {isBalanceVisible ? `R$ ${balance.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}` : '●●●,●●'}
+</p>
+                </div>
                 </div>
             </div>
         </div>
