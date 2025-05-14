989997f80c6c38020a5e80291c8a90847bd705fc
AlanGustav0 <alagustavo946@gmail.com>
2025-05-14 20:47:00 -0300

    feature: criação de link e Inicio

--- src/app/components/Sidebar/Sidebar.js
+++ src/app/components/Sidebar/Sidebar.js
@@ -1,10 +1,11 @@
 import './Sidebar.css';
+import Link from 'next/link';
 
 export default function Sidebar() {
     return (
         <div className="sidebar">
                     <ul className="menu-list">
-                        <li className="menu-item">Início</li>
+                        <li className="menu-item"><Link href="/financeiro/pageUser"> Início</Link></li>
                         <hr />
                         <li className="menu-item">Transferências</li>
                         <hr />
