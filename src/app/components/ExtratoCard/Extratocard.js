1b8709bbe384851df84864abd04262bc80c357f7
AlanGustav0 <alagustavo946@gmail.com>
2025-05-28 20:06:17 -0300

    feature: Id em extrato component

--- src/app/components/ExtratoCard/Extratocard.js
+++ src/app/components/ExtratoCard/Extratocard.js
@@ -1,36 +1,53 @@
-import './ExtratoCard.css'; // Estilos
+import "./ExtratoCard.css"; // Estilos
+import { getAccountUserById } from "../../financeiro/util-services";
+import React, { useState, useEffect } from "react";
+import { getformattedDate, getMonthName } from "../../shared/utils/date-utils.ts"
 
-export default function ExtratoCard() {
-    return (
-        <div className="extrato-card">
-                        <div className='headerExtrato'>
-                            <h2>Extrato</h2>
-                            <button className='botaoEditar'></button>
-                            <button className='botaoExcluir'></button>
-                        </div>
-                        
-                        <div className="extrato-item">
-                            <div className='mesExtrato'>
-                                <span>Novembro</span>
-                                <input type="checkbox" />
-                            </div>
-                            <div className='depositoExtrato'>
-                                <span>Depósito</span>
-                                <span className='dataExtrato'>20/11/2035</span>
-                            </div>                     
-                            <span className='valorExtrato'>R$ 150</span>
+export default function ExtratoCard(userId) {
+    const [account, setAccount] = useState({
+    userName: "",
+    saldo: 0,
+    extrato: [],
+  });
 
-                            <div className='mesExtrato'>
-                                <span>Dezembro</span>
-                                <input type="checkbox" />
-                            </div>
+  useEffect(() => {
+      getAccountUserById(userId.id).then((data) => {
+        if (data)
+          setAccount({
+            userName: data.userName,
+            saldo: data.saldo,
+            extrato: data.extrato,
+          });
+      });
+    }, [userId.id]);
 
-                            <div className='depositoExtrato'>
-                                <span>Depósito</span>
-                                <span className='dataExtrato'>20/11/2035</span>
-                            </div>                     
-                            <span className='valorExtrato'>R$ 150</span>
-                        </div>
-                    </div>
-    )
+  console.log("Extrato:", account);
+
+  return (
+    <div className="extrato-card">
+      <div className="headerExtrato">
+        <h2>Extrato</h2>
+        <button className="botaoEditar"></button>
+        <button className="botaoExcluir"></button>
+      </div>
+
+      {account.extrato.map((item, index) => (
+        <div className="extrato-item" key={index}>
+          <div className="mesExtrato">
+            <span>{getMonthName(new Date(item.data))}</span>
+            <input type="checkbox" />
+          </div>
+          <div className="depositoExtrato">
+            <span>{item.tipo}</span>
+            <span className="dataExtrato">{getformattedDate(new Date(item.data))}</span>
+          </div>
+          <span className="valorExtrato">{item.valor ? `R$ ${item.valor.toLocaleString("pt-BR", {
+              minimumFractionDigits: 2,
+              maximumFractionDigits: 2,
+            })}`
+          : "0,00"}</span>
+        </div>
+      ))}
+    </div>
+  );
 }
